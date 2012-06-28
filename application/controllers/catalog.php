732fcf30ac6ee33d01c046700defc90463d87355
Sophia Nepochataya <sophia.nepochataya@gmail.com>
2012-06-28 12:13:51 +0300

    ??????? ?????????

---
+++ application/controllers/catalog.php
@@ -0,0 +1,45 @@
+﻿<?php
+
+class Catalog extends CI_Controller{
+	
+	public function __construct() 
+    { 
+        parent::__construct();
+		$this->load->helper(array('url', 'html'));	
+	}
+	
+	function index()
+	{
+		category(0);
+	}
+	
+	function category($cat_id)
+	{
+		//Получаем название текущей категории
+		if ($cat_id) //Берем из БД
+		{
+			$query = $this->db->get_where('categories', array('id' => $cat_id));
+			$result = $query->row();
+			$data['title'] = $result->category;
+		}
+		else $data['title'] = 'Каталог'; //Если id=0, то корневой каталог
+		
+		//Получаем содержимое категории
+		$data['content'] = $this->db->get_where('categories', array('parent_id' => $cat_id))->result();
+		
+		//Путь к категории
+		$path = array();
+		while ($cat_id > 0)
+		{
+			$path_query = $this->db->get_where('categories', array('id' => $cat_id))->row();
+			$path[$cat_id] = $path_query->category;
+			$cat_id = $path_query->parent_id;
+		}
+		$path['0'] = 'Каталог';
+		$data['path'] = array_reverse($path, true); //Инверсия массива, чтобы категории шли по порядку
+		
+		$this->load->view('category_view', $data);
+	}
+}
+
+?>
---
+++ application/views/category_view.php
@@ -0,0 +1,41 @@
+﻿<html>
+<head>
+	<meta charset="utf-8">
+	<title><?=$title;?></title>
+	<?=link_tag('/data/css/style.css');?>
+</head>
+<body>
+	<div id = "page">
+	<div id = "header"></div>
+	<div id = "content">
+		<h1><?=$title;?></h1>
+		<?php
+		//Выводим путь к категории
+		$parents = count($path); //Кол-во родителей категории
+		$counter = 0;					// Счетчик для цикла. Криво, но работает
+		foreach($path as $id => $category) 
+		{ 	
+			$counter++;
+			if($counter==$parents) //Если последний, то просто текст без стрелочки
+			{
+				echo "<b>",$category,"</b>";
+			}
+			else //Если элемент не последний, то добавляем ссылку на категорию и стрелочки
+			{
+				echo "<b>", anchor('catalog/category/'.$id, $category)." -> </b>"; 
+			}	
+		} 
+		
+		//Выводим содержимое категории
+		echo "<ul>";
+		foreach ($content as $row)
+		{
+			echo "<li>", anchor('catalog/category/'.$row->id, $row->category), "</li>";
+		}
+		echo "</ul>";
+		?>
+	</div>
+	<div id = "footer"></div>
+	</div>
+</body>
+</html>
